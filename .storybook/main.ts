8a2af87e6783275f2ba84160e50d11e2effcf086
hari-i2 <hariharan_p@novactech.in>
2025-11-17 22:15:05 +0530

    feat: story fixes done

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,36 @@
-import type { StorybookConfig } from '@storybook/nextjs'
+import type { StorybookConfig } from '@storybook/react-vite'
+import { mergeConfig } from 'vite'
+import path from 'path'
 
 const config: StorybookConfig = {
-  stories: ['../packages/**/src/**/*.stories.@(js|jsx|ts|tsx|mdx)', '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: [
+    '../packages/*/src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../apps/*/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+  ],
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
-    '@storybook/addon-viewport'
+    '@storybook/addon-viewport',
+    '@storybook/addon-controls',
+    '@storybook/addon-actions',
+    '@storybook/addon-docs'
   ],
   framework: {
-    name: '@storybook/nextjs',
+    name: '@storybook/react-vite',
     options: {}
   },
   typescript: {
     check: false,
     reactDocgen: 'react-docgen-typescript'
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '../packages')
+        }
+      }
+    })
   }
 }
 
