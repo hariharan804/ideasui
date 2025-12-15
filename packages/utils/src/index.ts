8d18a1895d18fdec9eab5b58c03a5abce06b2cee
hariharn804 <hariharan804804@gmail.com>
2025-12-15 23:57:57 +0530

    fix: base utlis add

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,4 +2,13 @@
 // Only stable utilities should be exported from here.
 // DO NOT export React internals here
 // DO NOT export ARIA internals here
+
+// Core utilities
 export { cn, toDataAttr, mergeProps } from './shared/utils'
+
+// Subpath exports (these should match the exports in package.json)
+export * from './shared'
+export * from './aria'
+export * from './dom'
+export * from './react'
+export * from './style'
