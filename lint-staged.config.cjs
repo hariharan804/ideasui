fc6698cf8a0c0e76d678ebb21e8a5831e2c07ba6
hariharn804 <hariharan804804@gmail.com>
2025-12-17 22:39:29 +0530

    test

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,32 +1,34 @@
 const {relative} = require("path");
 const escape = require("shell-quote").quote;
 const isWin = process.platform === "win32";
-
 const {ESLint} = require("eslint");
 
 const removeIgnoredFiles = async (files) => {
   const cwd = process.cwd();
   const eslint = new ESLint();
+
   const relativePaths = files.map((file) => relative(cwd, file));
   const isIgnored = await Promise.all(relativePaths.map((file) => eslint.isPathIgnored(file)));
-  const filteredFiles = files.filter((_, i) => !isIgnored[i]);
 
-  return filteredFiles;
+  return files.filter((_, i) => !isIgnored[i]);
 };
 
 module.exports = {
   "**/*.{js,ts,jsx,tsx}": async (files) => {
     const filesToLint = await removeIgnoredFiles(files);
 
-    return filesToLint
-      .map((filename) => `"${isWin ? filename : escape([filename])}"`)
-      .map((file) => `eslint --max-warnings=0 --fix ${file}`);
+    return filesToLint.map((filename) => {
+      const file = isWin ? filename : escape([filename]);
+      return `eslint --max-warnings=0 --fix "${file}"`;
+    });
   },
+
   "**/*.css": async (files) => {
     const filesToLint = await removeIgnoredFiles(files);
 
-    return filesToLint
-      .map((filename) => `"${isWin ? filename : escape([filename])}"`)
-      .map((file) => `prettier --config .prettierrc --ignore-path .prettierignore --write ${file}`);
+    return filesToLint.map((filename) => {
+      const file = isWin ? filename : escape([filename]);
+      return `prettier --write "${file}"`;
+    });
   },
 };
