83ee3498f288c3dfa8c937dc4cc28531aa913f3d
hariharn804 <hariharan804804@gmail.com>
2025-12-30 23:50:07 +0530

    theme changes done

--- packages/core/theme/src/tokens/system.ts
+++ packages/core/theme/src/tokens/system.ts
@@ -1,10 +1,29 @@
-export { spacing } from './spacing';
-export { borderRadius } from './border-radius';
-export { fontSize } from './font-size';
-export { boxShadow } from './box-shadow';
-export { animation } from './animation';
-export { keyframes } from './keyframes';
-export { transitionDuration, transitionTimingFunction } from './transition';
+import {animation} from "./animation";
+import {borderRadius} from "./border-radius";
+import {boxShadow} from "./box-shadow";
+import {fontSize} from "./font-size";
+import {keyframes} from "./keyframes";
+import {spacing} from "./spacing";
+import {transitionDuration, transitionTimingFunction} from "./transition";
+
+// export {spacing} from "./spacing";
+// export {borderRadius} from "./border-radius";
+// export {fontSize} from "./font-size";
+// export {boxShadow} from "./box-shadow";
+// export {animation} from "./animation";
+// export {keyframes} from "./keyframes";
+// export {transitionDuration, transitionTimingFunction} from "./transition";
+
+export {
+  spacing,
+  borderRadius,
+  fontSize,
+  boxShadow,
+  animation,
+  keyframes,
+  transitionDuration,
+  transitionTimingFunction,
+};
 
 export const systemTokens = {
   spacing,
