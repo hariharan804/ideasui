205db7dc51d25815da42c814c9c1c181cbef2b59
hariharn804 <hariharan804804@gmail.com>
2026-01-03 13:45:51 +0530

    folder changes done

--- packages/core/theme-old/src/theme-controller.tsx
+++
@@ -1,9 +0,0 @@
-import type {ThemeConfig} from "./types";
-
-import {useThemeController} from "./use-theme-controller";
-
-export function ThemeController(props: Partial<ThemeConfig>) {
-  useThemeController(props);
-
-  return null; // no UI
-}
