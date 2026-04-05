c644c6ea40e01666e13efa9c086797d2cf9aa72d
hariharan804 <hariharan804804@gmail.com>
2026-04-05 18:06:21 +0530

    refactor: migrate storybook configuration and addons from v2 to current app structure

--- apps/storybook-old/.storybook/plugin.ts
+++
@@ -1,6 +0,0 @@
-import { ideasUIPlugin } from '@ideasui/theme';
-import type { Config } from 'tailwindcss';
-
-export default ideasUIPlugin({
-  defaultTheme: 'light',
-}) as unknown as Config;
--- apps/storybook-old/.storybook/vitest.setup.ts
+++
@@ -1,5 +0,0 @@
-import { setProjectAnnotations } from '@storybook/react';
-import * as projectAnnotations from './preview';
-
-// This is required to initialize Storybook's rendering engine in Vitest.
-setProjectAnnotations(projectAnnotations);
