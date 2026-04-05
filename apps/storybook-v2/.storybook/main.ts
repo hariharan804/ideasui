1a25fa8e846029d689ef662d6d51b7b94cfbd0fc
hariharan804 <hariharan804804@gmail.com>
2026-04-05 17:59:08 +0530

    feat: add @chromatic-com/storybook addon to storybook configuration

--- apps/storybook-v2/.storybook/main.ts
+++ apps/storybook-v2/.storybook/main.ts
@@ -20,21 +20,16 @@ const config: StorybookConfig = {
 
   staticDirs: ['../public'],
   refs: {},
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        mdxPluginOptions: {
-          mdxCompileOptions: {
-            remarkPlugins: [remarkGfm],
-          },
+  addons: ['@storybook/addon-links', '@storybook/addon-a11y', {
+    name: '@storybook/addon-docs',
+    options: {
+      mdxPluginOptions: {
+        mdxCompileOptions: {
+          remarkPlugins: [remarkGfm],
         },
       },
     },
-    '@storybook/addon-vitest',
-  ],
+  }, '@storybook/addon-vitest', '@chromatic-com/storybook'],
 
   core: { disableTelemetry: true },
 
